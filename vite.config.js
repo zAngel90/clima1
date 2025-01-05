fea1a72edaad17cb23061a164996c25117b32790
zAngel90 <av020431@gmail.com>
2025-01-05 18:00:46 -0500

    fix: Simplify configuration and use relative paths

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/clima/',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      },
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
+    outDir: 'dist'
   }
 }) 
