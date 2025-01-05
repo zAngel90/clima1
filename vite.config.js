8de2040131bb890cea2f5a0673294fe5362f646e
zAngel90 <av020431@gmail.com>
2025-01-05 18:07:05 -0500

    Update configuration for new repository

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/clima/',
+  base: '/clima1/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
