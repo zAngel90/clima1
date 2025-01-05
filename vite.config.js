473a60fd58276903b424637793af5a7e11bf62ac
zAngel90 <av020431@gmail.com>
2025-01-05 18:24:23 -0500

    fix: Simplify configuration and fix paths

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/clima1/',
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      },
-      output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
+    outDir: 'dist'
   }
 }) 
