4b2ec3451c8bfbf86c25e49a6f652cceb5782f66
zAngel90 <av020431@gmail.com>
2025-01-05 17:39:56 -0500

    Initial commit: Weather App with static build

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/clima/',
+  base: './',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
+    outDir: 'dist'
   }
 }) 
